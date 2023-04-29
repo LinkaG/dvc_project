7b9064308a211e08c23cdd76ff21e5f6049a1d25
LinkaG <z.gilvanova@yandex.ru>
2023-04-29 07:11:29 +0500

    Начало работы с DVC: настройка и загрузка DVC-файлов

---
+++ src/data/load_data.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+import pandas as pd
+import numpy as np
+import click
+
+
+@click.command()
+@click.argument("output_path", type=click.Path())
+def load_data(output_path: str):
+    """ Function load init dataset.
+    :param output_path: Path to save cleaned DataFrame
+    :return:
+    """
+    iris = datasets.load_iris()
+    iris_df = pd.DataFrame(
+        data=np.c_[iris['data'], iris['target']],
+        columns=iris['feature_names'] + ['target']
+    )
+    iris_df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    load_data()
